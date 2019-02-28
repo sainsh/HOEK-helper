d5e3c8602660ce6a18c0d468455d93334dcd6077
MathiasAmi <math35i0@edu.easj.dk>
2019-02-28 09:54:25 +0100

    Added interface Added Model and imp class

--- 05 Implementation/AndroidProject/app/src/main/java/com/example/hoek_helper/Model/angivVE.java
+++ 05 Implementation/AndroidProject/app/src/main/java/com/example/hoek_helper/Model/angivVE.java
@@ -1,4 +1,7 @@
 package com.example.hoek_helper.Model;
 
 public interface angivVE {
+
+    void setVaerdi(double Vaerdi);
+    double getVaerdi();
 }
---
+++ 05 Implementation/AndroidProject/app/src/main/java/com/example/hoek_helper/Model/angivVEImp.java
@@ -0,0 +1,15 @@
+package com.example.hoek_helper.Model;
+
+public class angivVEImp implements angivVE {
+
+    private double vaerdi;
+
+    public void setVaerdi(double vaerdi){
+        this.vaerdi = vaerdi;
+    }
+
+    public double getVaerdi(){
+        return vaerdi;
+    }
+
+}
