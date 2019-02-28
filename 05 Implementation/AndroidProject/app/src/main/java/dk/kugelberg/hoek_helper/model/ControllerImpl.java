0b2e956ba7895ea275fd945d16fad9af01e8bdb5
sainsh <kfsainsh@gmail.com>
2019-02-28 12:39:56 +0100

    fixed issues with package domain, outcommented all but functions in controller

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/ControllerImpl.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/ControllerImpl.java
@@ -1,40 +1,43 @@
 package dk.kugelberg.hoek_helper.model;
 
 public class ControllerImpl implements Controller {
-
+/*
     KO ko;
     X x1;
     X x2;
     VO vo1;
     VO vo2;
     DOMK domk;
-
+*/
     public ControllerImpl() {
 
-        ko = new KO();
+     //   ko = new KO();
     }
 
 
     @Override
     public void angivKO(double vaerdi) {
-        return ko.setVaerdi(vaerdi);
+        //ko.setVaerdi(vaerdi);
+
     }
 
     @Override
     public double hentKO() {
-        return ko.getVaerdi();
+       // return ko.getVaerdi();
+        return 0.0;
     }
 
     @Override
     public void beregnDOMK() {
 
-        domk = new DOMKImpl();
-        domk.setVaerdi(x1.getAntal,x2.getAntal,vo1.getVaerdi,vo2.getVaerdi);
+       // domk = new DOMKImpl();
+       // domk.setVaerdi(x1.getAntal,x2.getAntal,vo1.getVaerdi,vo2.getVaerdi);
 
     }
 
     @Override
     public double hentDOMK() {
-        return domk.getDOMK();
+       // return domk.getDOMK();
+        return 0.0;
     }
 }
--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/view/MainActivity.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/view/MainActivity.java
@@ -3,7 +3,7 @@ package dk.kugelberg.hoek_helper.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.hoek_helper.R;
+import dk.kugelberg.hoek_helper.R;
 
 public class MainActivity extends AppCompatActivity {
 
