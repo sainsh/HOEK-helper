90af2170b4d0704b4627ee52a72ce70b654bca49
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2019-02-28 13:49:52 +0100

    added TestingXImpl

---
+++ 05 Implementation/AndroidProject/app/src/test/java/com/example/hoek_helper/TestingXlmpl.java
@@ -0,0 +1,20 @@
+package com.example.hoek_helper;
+
+import com.example.hoek_helper.Model.XImpl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestingXlmpl {
+    XImpl t=new XImpl();
+    @Test
+    public void setAntal(){
+        t.setAntal(-5);
+    }
+    @Test
+    public void getAntal(){
+        t.setAntal(10);
+        assertEquals(10,t.getAntal());
+    }
+}
