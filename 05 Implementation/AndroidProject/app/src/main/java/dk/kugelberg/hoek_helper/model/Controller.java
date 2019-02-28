b98ceaf3292bcec456c0397c5445dc591314edeb
sainsh <kfsainsh@gmail.com>
2019-02-28 12:16:41 +0100

    changed android version to 26.1.0 added packages changed package domain to dk.kugelberg

---
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/Controller.java
@@ -0,0 +1,17 @@
+package dk.kugelberg.hoek_helper.model;
+
+public interface Controller {
+
+
+    void angivKO(double vaerdi);
+
+    double hentKO();
+
+    void beregnDOMK();
+
+    double hentDOMK();
+
+
+
+
+}
