a217bdb51b09495e39391c5db1c7349e96e8af61
Lucas Kugelberg <kugelberg.dk@gmail.com>
2019-02-28 15:32:37 +0100

    Added data fields and methods

---
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/DOMK.java
@@ -0,0 +1,13 @@
+package dk.kugelberg.hoek_helper.model;
+
+
+public interface DOMK {
+
+    void setVaerdi(double vaerdi);
+    double getVaerdi();
+    void beregn();
+    void init(VO vo1, VO vo2, STO sto, KO ko, VE ve, X x1, X x2, DOMK domk1, DOMK domk2);
+    boolean erBeregnet();
+
+
+}
---
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/DOMKImpl.java
@@ -0,0 +1,42 @@
+package dk.kugelberg.hoek_helper.model;
+
+public class DOMKImpl implements DOMK {
+
+    private VO vo1;
+    private VO vo2;
+    private STO sto;
+    private KO ko;
+    private VE ve;
+    private X x1;
+    private X x2;
+    private DOMK domk1;
+    private DOMK domk2;
+    private double vaerdi = Double.NaN;
+    private boolean erBeregnet = false;
+
+    @Override
+    public void setVaerdi(double vaerdi) {
+
+    }
+
+    @Override
+    public double getVaerdi() {
+        return 0;
+    }
+
+    @Override
+    public void beregn() {
+
+    }
+
+    @Override
+    public void init(VO vo1, VO vo2, STO sto, KO ko, VE ve, X x1, X x2, DOMK domk1, DOMK domk2) {
+
+    }
+
+    @Override
+    public boolean erBeregnet() {
+
+        return false;
+    }
+}
