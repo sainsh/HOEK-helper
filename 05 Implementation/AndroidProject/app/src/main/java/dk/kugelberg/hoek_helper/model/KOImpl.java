7c4be8b94bf906ea989834a207c56d3b98e2a705
sainsh <kfsainsh@gmail.com>
2019-02-28 16:02:54 +0100

    changed Package

---
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/KOImpl.java
@@ -0,0 +1,14 @@
+package dk.kugelberg.hoek_helper.model;
+
+public class KOImpl implements KO {
+
+    private double vaerdi;
+
+    public void setVaerdi(double vaerdi) {
+        this.vaerdi = vaerdi;
+    }
+
+    public double getVaerdi() {
+        return vaerdi;
+    }
+}
