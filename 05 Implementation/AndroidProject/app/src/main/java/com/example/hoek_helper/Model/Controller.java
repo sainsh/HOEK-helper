c7ccd4929ccc51c78d9acf1c0b9809e17649f293
sainsh <kfsainsh@gmail.com>
2019-02-28 10:59:22 +0100

    implemented SD_05 implemented SD_07

--- 05 Implementation/AndroidProject/app/src/main/java/com/example/hoek_helper/Model/Controller.java
+++ 05 Implementation/AndroidProject/app/src/main/java/com/example/hoek_helper/Model/Controller.java
@@ -1,4 +1,17 @@
 package com.example.hoek_helper.Model;
 
 public interface Controller {
+
+
+    void angivKO(double vaerdi);
+
+    double hentKO();
+
+    void beregnDOMK();
+
+    double hentDOMK();
+
+
+
+
 }
