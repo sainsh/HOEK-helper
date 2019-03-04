9eef272135a61585c604df5bd3054a5b828e9ce7
tobias <harly.grunewald@gmail.com>
2019-03-04 11:26:49 +0100

    changed interface to class

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/KEimpl.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/KEimpl.java
@@ -2,16 +2,20 @@ package dk.kugelberg.hoek_helper.model;
 
 import static java.lang.Double.NaN;
 
-public interface KEimpl extends KE {
+public class KEimpl extends KE {
 
     private double vaerdi;
 
     public void setVaerdi(double vaerdi){
+
         this.vaerdi = vaerdi;
+
     }
 
     public double getVaerdi(){
+
         return vaerdi;
+
     }
 
     @Override
