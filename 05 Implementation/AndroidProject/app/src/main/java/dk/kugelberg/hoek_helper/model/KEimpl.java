bb7fe9a2769dbfa7aad71546013a579a820f4b41
tobias <harly.grunewald@gmail.com>
2019-03-04 13:27:02 +0100

    updated

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/KEimpl.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/KEimpl.java
@@ -6,12 +6,35 @@ public class KEimpl extends KE {
 
     private double vaerdi;
 
+    private KO ko;
+    private X x;
+
+    private SE se;
+    private VE ve;
+
+    private boolean erBeregnet = false;void init(KO ko, X x, SE se, VE ve);
+
+    @Override
+    public void init(KO ko, X x, SE se, VE ve){
+        this.ko = ko;
+        this.x = x;
+        this.se = se;
+        this.ve = ve;
+    }
+
+    @Override
     public void setVaerdi(double vaerdi){
 
-        this.vaerdi = vaerdi;
+        if (vaerdi < 0) {
+            throw new NegativVaerdiException();
+        } else {
+            this.vaerdi = vaerdi;
+            erBeregnet = false;
+        }
 
     }
 
+    @Override
     public double getVaerdi(){
 
         return vaerdi;
@@ -19,7 +42,35 @@ public class KEimpl extends KE {
     }
 
     @Override
+    private void setBeregnet(boolean val){
+        erBeregnet = val;
+    }
+
+    @Override
+    public boolean getBeregnet(){
+        return erBeregnet;
+    }
+
+    @Override
     public void beregn() {
+
+        if(ko.getVaerdi() != NaN && x.getVaerdi() != NaN){
+
+            this.vaerdi = ko.getVaerdi() / x.getVaerdi();
+            setBeregnet(true);
+
+        }
+        else if(se.getVaerdi() != NaN && ve.getVaerdi() != NaN){
+
+            this.vaerdi = se.getVaerdi() - ve.getVaerdi();
+            setBeregnet(true);
+
+        }
+        else if(getBeregnet()){
+
+            this.vaerdi = NaN;
+
+        }
 
     }
 
