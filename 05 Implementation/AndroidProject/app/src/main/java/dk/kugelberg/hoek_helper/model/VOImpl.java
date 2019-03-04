e2ba9289839eb2788c92746ae24263f2a8d7f053
Burhan Öztürk <burh0125@edu.easj.dk>
2019-03-04 10:31:32 +0100

    Edited XImpl

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VOImpl.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VOImpl.java
@@ -1,6 +1,7 @@
 package dk.kugelberg.hoek_helper.model;
 
-public class VOImpl implements VO {
+public class VOImpl implements VO
+{
 
 
     private double vaerdi = Double.NaN;
@@ -11,36 +12,33 @@ public class VOImpl implements VO {
 
     }
 
+    public void beregn()
+    {
+
+    }
+
 
 
     @Override
     public void setVaerdi(double vaerdi)
     {
-        if(vaerdi<0)
-        {
+        if(vaerdi<0) {
             throw new VaerdiException();
+        }
         else {
             this.vaerdi = vaerdi;
             erBeregnet=false;
         }
     }
 
+
     @Override
-    public void beregn()
+    public double getVaerdi()
     {
-
-
-
-
-
-
-        //this.vaerdi = x;
-    }
-
-    @Override
-    public double getVaerdi() {
         if (vaerdi == Double.NaN)
+        {
             beregn();
+        }
         return vaerdi;
     }
 }
