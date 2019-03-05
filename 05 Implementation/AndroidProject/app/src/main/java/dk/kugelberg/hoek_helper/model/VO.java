d362579533a4831627a235705d454020ec2616a9
Christian T Flink <chri62aa@edu.easj.dk>
2019-03-05 09:58:11 +0100

    VE og X 2

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VO.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VO.java
@@ -3,24 +3,19 @@ package dk.kugelberg.hoek_helper.model;
 
 public interface VO {
 
-    /* ****************************************************************************************
-     * *********************************GETTERS AND SETTERS************************************
-     * ***************************************************************************************/
+    void init(VE ve, X x, KO ko, DOMK domk, STO sto, SE se);
+    void init1(X x1, VO vo1);
+    void init2(X x2, VO vo2, DOMK domk2);
 
-
-    /**
-     * Sætter variablen vaerdi til værdien angivet som argument.
-     * @param vaerdi
-     */
     void setVaerdi(double vaerdi);
 
-    /**
-     * Returnerer værdien af variablen vaerdi.
-     * @return
-     */
     double getVaerdi();
 
-    //void beregn();
+    void beregn();
+
+    void setBeregnet(boolean val);
+
+    boolean getBeregnet();
 
 
 }
--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VOImpl.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/VOImpl.java
@@ -1,7 +1,8 @@
 package dk.kugelberg.hoek_helper.model;
 
-public class VOImpl implements VO
-{
+import static java.lang.Double.NaN;
+
+public class VOImpl implements VO {
 
     VE ve;
     X x;
@@ -25,7 +26,7 @@ VO = DB - Oms
     private boolean erBeregnet = false;
 
 
-    public void init(VE ve, X x, KO ko,, DOMK domk, STO sto, SE se) {
+    public void init(VE ve, X x, KO ko, DOMK domk, STO sto, SE se) {
         this.ve = ve;
         this.x = x;
         this.ko = ko;
@@ -35,34 +36,71 @@ VO = DB - Oms
     }
 
     @Override
-    public void init1(X x1, VO vo1){
+    public void init1(X x1, VO vo1) {
         this.x1 = x1;
         this.vo1 = vo1;
     }
 
     @Override
-    public void init2(X x2, VO vo2, DOMK domk2){
+    public void init2(X x2, VO vo2, DOMK domk2) {
         this.x2 = x2;
         this.vo2 = vo2;
         this.domk2 = domk2;
     }
 
     @Override
-    public void setVaerdi(double vaerdi)
-    {
-        if(vaerdi<0) {
+    public void setVaerdi(double vaerdi) {
+        if (vaerdi < 0) {
             throw new VaerdiException();
-        }
-        else {
+        } else {
             this.vaerdi = vaerdi;
-            erBeregnet=false;
+            erBeregnet = false;
         }
+    }
+
+    @Override
+    public double getVaerdi() {
+        return vaerdi;
+    }
+
+    @Override
+    public void setBeregnet(boolean val){
+        erBeregnet = val;
+    }
+
+    @Override
+    public boolean getBeregnet(){
+        return erBeregnet;
     }
 
 
     @Override
-    public double getVaerdi()
-    {
-        return vaerdi;
+    public void beregn() {
+
+
+        if (ve.getVaerdi() != NaN && x.getVaerdi() != NaN) {
+            this.vaerdi = ve.getVaerdi() * x.getVaerdi();
+            setBeregnet(true);
+
+        } else if (sto.getVaerdi() != NaN && ko.getVaerdi() != NaN) {
+            this.vaerdi = sto.getVaerdi() - ko.getVaerdi();
+            setBeregnet(true);
+
+        } /* else if (db.getVaerdi() != NaN && oms.getVaerdi() != NaN) {
+            this.vaerdi = db.getVaerdi() - oms.getVaerdi();
+            setBeregnet(true);
+
+        } */ else if (getBeregnet()) {
+
+            this.vaerdi = NaN;
+
+        }
+
     }
 }
+
+/*
+VO = VE * X
+VO = STO - KO
+VO = DB - Oms
+*/
