9869eb99fbf34c69c1d4d22ecbaeaf2295c8f3b7
sainsh <kfsainsh@gmail.com>
2019-03-07 09:45:17 +0100

    fixed beregn() added initOver and MutableLiveData

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/DOMK.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/model/DOMK.java
@@ -6,8 +6,9 @@ public interface DOMK {
     void setVaerdi(double vaerdi);
     double getVaerdi();
     void beregn();
-    void init(VO vo1, VO vo2, STO sto, KO ko, VE ve, X x1, X x2, DOMK domk1, DOMK domk2);
+    void init(VO vo, VO voOver, STO sto, KO ko, VE ve, X x, X xOver, DOMK domk, DOMK domkOver);
     boolean erBeregnet();
+    void initOver(VO voOver,X xOver, DOMK domkOver);
 
 
 }
