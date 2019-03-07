ad058fb630e49e6beda30e36b62da89db163d56e
Dalphs <42994000+Dalphs@users.noreply.github.com>
2019-03-07 13:20:54 +0100

    sync

--- 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/view/android/RowFragment.java
+++ 05 Implementation/AndroidProject/app/src/main/java/dk/kugelberg/hoek_helper/view/android/RowFragment.java
@@ -38,61 +38,13 @@ public class RowFragment extends Fragment {
                              Bundle savedInstanceState) {
         // xEditText = getView().findViewById(R.id.x_text_view);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_row, container, false);
-    }
-
-    public void initTextViews(){
-        xEditText = (EditText) getView().findViewById(R.id.x_text_view);
-        /*xEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() > 0){
-                    x = Integer.parseInt(s.toString());
-                    beregn();
-                } else{
-                    x = Double.NaN;
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });*/
-
-        voEditText = (EditText) getView().findViewById(R.id.vo_text_view);
-        /*voEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() > 0){
-                    vo = Double.parseDouble(s.toString());
-                    System.out.println(vo);
-                    beregn();
-                } else{
-                    vo = Double.NaN;
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });*/
-
-        veEditText = (EditText) getView().findViewById(R.id.ve_text_view);
-
-        domkEditText = (EditText) getView().findViewById(R.id.domk_text_view);
+        View v = inflater.inflate(R.layout.fragment_row, container, false);
+        xEditText = v.findViewById(R.id.x_text_view);
+        xEditText.addTextChangedListener(xFieldWatcher);
+        voEditText = v.findViewById(R.id.vo_text_view);
+        voEditText.addTextChangedListener(voTextWatcher);
+        domkEditText = v.findViewById(R.id.domk_text_view);
+        return v;
     }
 
     public void beregn(){
@@ -102,8 +54,50 @@ public class RowFragment extends Fragment {
         }
     }
 
+    TextWatcher xFieldWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+        }
 
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() > 0){
+                x = Integer.parseInt(s.toString());
+                beregn();
+            } else{
+                x = Double.NaN;
+            }
+        }
 
+        @Override
+        public void afterTextChanged(Editable s) {
 
-}
+        }
+    };
+
+    TextWatcher voTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(s.length() > 0){
+                vo = Double.parseDouble(s.toString());
+                System.out.println(vo);
+                beregn();
+            } else{
+                vo = Double.NaN;
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+ }
