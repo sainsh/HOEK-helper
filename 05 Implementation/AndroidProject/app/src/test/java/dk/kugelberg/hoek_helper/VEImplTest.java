5fc6c4c386e02b59de4593bab27d1c1ec704a41d
EmilDino <EmilLD123@hotmail.com>
2019-03-11 14:43:05 +0100

    lavet test for VO/X, SE-KE og test for negativ VO

--- 05 Implementation/AndroidProject/app/src/test/java/dk/kugelberg/hoek_helper/VEImplTest.java
+++ 05 Implementation/AndroidProject/app/src/test/java/dk/kugelberg/hoek_helper/VEImplTest.java
@@ -2,6 +2,7 @@ package dk.kugelberg.hoek_helper;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import dk.kugelberg.hoek_helper.model.KE;
+import dk.kugelberg.hoek_helper.model.NegativVaerdiException;
 import dk.kugelberg.hoek_helper.model.SE;
 import dk.kugelberg.hoek_helper.model.VE;
 import dk.kugelberg.hoek_helper.model.VEImpl;
@@ -38,17 +39,52 @@ public class VEImplTest {
      */
 
     // kontrollerer om VO/X formlen virker
-    /**
     @Test
-    public void testBeregnMedVOFormel() {
+    public void testBeregnVEMedVOFormel() {
         VEImpl ve = new VEImpl();
-        ve.init(new VEMock(Double.NaN), new VOMock(Double.NaN), );
+        ve.init(new MOCKS.VOMock(5), new MOCKS.XMock(5), new MOCKS.SEMock(Double.NaN), new MOCKS.KEMock(Double.NaN));
+        double resultat = 1;
+        ve.beregn();
+        assertEquals(ve.getVaerdi(), resultat, delta);
+        assertTrue(ve.getBeregnet());
+    }
+
+    // kontrollerer om SE - KE formlen virker
+    @Test
+    public void testBeregnVEMedSEKEFormel() {
+        VEImpl ve = new VEImpl();
+        ve.init(new MOCKS.VOMock(Double.NaN), new MOCKS.XMock(Double.NaN), new MOCKS.SEMock(3), new MOCKS.KEMock(2));
+        double resultat = 1;
+        ve.beregn();
+        assertEquals(ve.getVaerdi(), resultat, delta);
+        assertTrue(ve.getBeregnet());
+    }
+
+    // kontrollerer om VE bliver udregnet når der mangler et tal
+    @Test
+    public void testBeregnVEMedManglendeX() {
+        VEImpl ve = new VEImpl();
+        ve.init(new MOCKS.VOMock(5), new MOCKS.XMock(Double.NaN), new MOCKS.SEMock(Double.NaN), new MOCKS.KEMock(Double.NaN));
         double resultat = Double.NaN;
         ve.beregn();
-        assertEquals();
-        assertFalse();
+        assertEquals(ve.getVaerdi(), resultat, delta);
+        assertFalse(ve.getBeregnet());
     }
-     */
+
+    // tester om VE bliver udregnet når der bliver indtastet et negativt tal
+    @Test
+    public void testBeregnVEMedNegativeTal() {
+        VEImpl ve = new VEImpl();
+        ve.init(new MOCKS.VOMock(-5), new MOCKS.XMock(5), new MOCKS.SEMock(Double.NaN), new MOCKS.KEMock(Double.NaN));
+        double resultat = Double.NaN;
+
+        try {
+            ve.beregn();
+        }catch (NegativVaerdiException nve) {
+            return;
+        } assertTrue(ve.getBeregnet());
+        fail();
+    }
 
     @Test
     public void setVaerdi() {
